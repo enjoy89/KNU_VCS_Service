4f26041e0bf95546666895f4a3ce4363cf4d2b37
jeondui <jeondui@gmail.com>
2023-07-12 11:30:04 +0900

    vcs controller 작성

--- backend/src/main/java/com/team4/backend/controller/VCSController.java
+++ backend/src/main/java/com/team4/backend/controller/VCSController.java
@@ -1,7 +1,20 @@
 package com.team4.backend.controller;
 
+import com.team4.backend.domain.vcs.entity.VCS;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/team4/vercontrol")
 public class VCSController {
+    private final
+
+    @GetMapping("/config-all")
+    public ResponseEntity<List<VCS>> getAllConfig() {
+
+    }
 }
