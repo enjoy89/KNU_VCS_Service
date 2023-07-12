d8b8e455c9a6a6302fe933dee14f09c71d721ebe
jeondui <jeondui@gmail.com>
2023-07-12 09:50:55 +0900

    Initial commit

---
+++ backend/src/main/java/com/team4/backend/TestController.java
@@ -0,0 +1,13 @@
+package com.team4.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello, world 4팀!";
+    }
+}
